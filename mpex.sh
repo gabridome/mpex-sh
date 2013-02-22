8f430ee45745250a74bb0d40472d996057d95a1e
Gabriele Domenichini <gabridome@fastmail.fm>
2013-02-22 23:48:25 +0000

    You can close the command between ''. added in the comments.
    if you write ./mpex.sh 'BUY|S.DICE|40000'
    you don't need to escape the "|" character.

--- mpex.sh
+++ mpex.sh
@@ -2,8 +2,9 @@
 # mpex.sh: Simple shell script interface to mpex.co
 # todo: url encode the command without the necessity of the escape "\" character.
 # usage example: ./mpex.sh BUY\|S.DICE\|40000
+# or: ./mpex.sh 'BUY|S.DICE|40000'
 # don't forget the "!" after your keyID.
-echo "$*" | gpg --clearsign -u yourKeyID! | gpg --encrypt --armor -r F1B69921 --trust-model always > out.txt
+echo $* | gpg --clearsign -u yourKeyID! | gpg --encrypt --armor -r F1B69921 --trust-model always > out.txt
 curl -s --data-urlencode msg@out.txt http://mpex.co >out.asc
 gpg --batch --yes out.asc
 cat out
